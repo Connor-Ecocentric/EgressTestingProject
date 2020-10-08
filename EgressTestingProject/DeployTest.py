216eccde57fcb9f651a77fa60dc7e29c71dec0f3
Connor Gregory <connor.gregory@ecocentric.energy>
2020-10-08 11:25:09 +0800

    Big upgrade to SSH_Comms

--- EgressTestingProject/DeployTest.py
+++ EgressTestingProject/DeployTest.py
@@ -19,16 +19,16 @@ class Collector():
         self.LocalFile1 = ("%s%s") % (cwd,'\\eco-feature-extract-serial-write')
         self.LocalFile2 = ("%s%s") % (cwd, '\\Collector_Files\\ConfigFix.sh')
         self.TestFile = ("%s%s") % (cwd,'\\EgressTestV2.py')
-
         self.RemotePath1 = '/home/root/test'
         self.RemotePath2 = '/'
         self.RemoteFile1 = '/home/root/test/N9C350B021801*' + str(TIMESTAMP) + '*'
+        self.sshComms = SSH_Comms.SSH()
     def SendFile(self): 
-        SSH_Comms.SSH().Connect(self.CollectorIp)
+        self.sshComms.Connect(self.CollectorIp)
         ### Check for logging directory ###
-        DirTest = SSH_Comms.SSH().SendCommand("ls -l /home/root/ | grep test | awk '{printf $9}'")
+        DirTest = self.sshComms.SendCommand("ls -l /home/root/ | grep test | awk '{printf $9}'")
         if  "test" not in DirTest: 
-            SSH_Comms.SSH().SendCommand("mkdir /home/root/test; mkdir /home/root/test/mem; mkdir /home/root/test/sdcard; mkdir /home/root/test/calibration")
+            self.sshComms.SendCommand("mkdir /home/root/test; mkdir /home/root/test/mem; mkdir /home/root/test/sdcard; mkdir /home/root/test/calibration")
             print("'Test' directory did not exist, it has now been created in the /home/root path")
             SSH_Comms.ssh.close()
             time.sleep(1)
@@ -38,47 +38,47 @@ class Collector():
 
         ### Send and Commence Testing ###
         
-            SSH_Comms.SSH().sendSCP(self.TestFile, self.RemotePath1)
-            SSH_Comms.SSH().sendDirectorySCP(self.LocalPath2, self.RemotePath1)
-            SSH_Comms.SSH().SendCommand("PATH=/usr/bin:/usr/local/bin:/sbin:/bin:/usr/sbin && python /home/root/test/EgressTestV2.py")
+            self.sshComms.sendSCP(self.TestFile, self.RemotePath1)
+            self.sshComms.sendDirectorySCP(self.LocalPath2, self.RemotePath1)
+            self.sshComms.SendCommand("PATH=/usr/bin:/usr/local/bin:/sbin:/bin:/usr/sbin && python /home/root/test/EgressTestV2.py")
             print("Completed EgessTestV2")
             SSH_Comms.ssh.close()
             return
     def GetSDhealth(self): 
-        SSH_Comms.SSH().Connect(self.CollectorIp)
-        SSH_Comms.SSH().SendCommand('/home/root/bin/SMART_Tool_Sample_armabihf /dev/mmcblk0 > $HOSTNAME.txt')
+        self.sshComms.Connect(self.CollectorIp)
+        self.sshComms.SendCommand('/home/root/bin/SMART_Tool_Sample_armabihf /dev/mmcblk0 > $HOSTNAME.txt')
         SSH_Comms.ssh.close()
 
     def GetFile(self):
-        SSH_Comms.SSH().Connect(self.CollectorIp)
-        SSH_Comms.SSH().getSCP(self.LocalPath1, self.RemoteFile1)
+        self.sshComms.Connect(self.CollectorIp)
+        self.sshComms.getSCP(self.LocalPath1, self.RemoteFile1)
         SSH_Comms.ssh.close()
     def ShutDown(self):
-        SSH_Comms.SSH().Connect(self.CollectorIp)
-        SSH_Comms.SSH().SendCommand("./mcu-disable-always-on.sh")
-        SSH_Comms.SSH().SendCommand("sync; shutdown -P -t now")
+        self.sshComms.Connect(self.CollectorIp)
+        self.sshComms.SendCommand("./mcu-disable-always-on.sh")
+        self.sshComms.SendCommand("sync; shutdown -P -t now")
         SSH_Comms.ssh.close()
     def SerialFix(self):
-        SSH_Comms.SSH().Connect(self.CollectorIp)
-        SSH_Comms.SSH().sendSCP(self.LocalFile1, self.RemotePath2)
-        SSH_Comms.SSH().SendCommand("chmod 777 /eco-feature-extract-serial-write")
-        SSH_Comms.SSH().SendCommand("/eco-feature-extract-serial-write -s $HOSTNAME")
-        SSH_Comms.SSH().SendCommand("rm /eco-feature-ectract-serial-write")
+        self.sshComms.Connect(self.CollectorIp)
+        self.sshComms.sendSCP(self.LocalFile1, self.RemotePath2)
+        self.sshComms.SendCommand("chmod 777 /eco-feature-extract-serial-write")
+        self.sshComms.SendCommand("/eco-feature-extract-serial-write -s $HOSTNAME")
+        self.sshComms.SendCommand("rm /eco-feature-ectract-serial-write")
         SSH_Comms.ssh.close()
     def ConfigFix(self):
-        SSH_Comms.SSH().Connect(self.CollectorIp)
-        SSH_Comms.SSH().sendSCP(self.LocalFile2, self.RemotePath1)
-        SSH_Comms.SSH().SendCommand("tr -d '\r' <ConfigFix.sh >ConfigFix.sh.new && mv ConfigFix.sh.new ConfigFix.sh")
-        SSH_Comms.SSH().SendCommand("chmod 755 /home/root/ConfigFix.sh")
-        SSH_Comms.SSH().SendCommand("/home/root/ConfigFix.sh")
-        SSH_Comms.SSH().SendCommand("rm /home/root/ConfigFix.sh")
+        self.sshComms.Connect(self.CollectorIp)
+        self.sshComms.sendSCP(self.LocalFile2, self.RemotePath1)
+        self.sshComms.SendCommand("tr -d '\r' <ConfigFix.sh >ConfigFix.sh.new && mv ConfigFix.sh.new ConfigFix.sh")
+        self.sshComms.SendCommand("chmod 755 /home/root/ConfigFix.sh")
+        self.sshComms.SendCommand("/home/root/ConfigFix.sh")
+        self.sshComms.SendCommand("rm /home/root/ConfigFix.sh")
         SSH_Comms.ssh.close()
     def UptimeCheck(self):
-        SSH_Comms.SSH().Connect(self.CollectorIp)
-        SSH_Comms.SSH().SendCommand("tr -d '\r' <ConfigFix.sh >ConfigFix.sh.new && mv ConfigFix.sh.new ConfigFix.sh")
-        SSH_Comms.SSH().SendCommand("chmod 755 /home/root/ConfigFix.sh")
-        SSH_Comms.SSH().SendCommand("/home/root/ConfigFix.sh")
-        SSH_Comms.SSH().SendCommand("rm /home/root/ConfigFix.sh")
+        self.sshComms.Connect(self.CollectorIp)
+        self.sshComms.SendCommand("tr -d '\r' <ConfigFix.sh >ConfigFix.sh.new && mv ConfigFix.sh.new ConfigFix.sh")
+        self.sshComms.SendCommand("chmod 755 /home/root/ConfigFix.sh")
+        self.sshComms.SendCommand("/home/root/ConfigFix.sh")
+        self.sshComms.SendCommand("rm /home/root/ConfigFix.sh")
         SSH_Comms.ssh.close()
 
 def notsure():
